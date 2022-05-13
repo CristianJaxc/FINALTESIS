41af00b6db9913923bab553524dbe7f49ec3059b
CristianJaxc <cristian.acosta4503@utc.edu.ec>
2022-05-13 02:08:42 -0500

    COMMIT SUBIDA7 05132022

--- sabia/sabia/settings.py
+++ sabia/sabia/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'sabia.wsgi.application'
 
 DATABASES = {
     'default': {
+
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'SabiaFinalDB',
             'USER': 'super',
